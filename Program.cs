2e45d09845f220eae9f899feccb488ec5be90478
jmtretter.jb <jmtretter.jb@hotmail.com>
2017-12-13 19:25:29 -0600

    Push

--- Program.cs
+++ Program.cs
@@ -299,7 +299,26 @@ namespace Enums
 
             
 
-           
+           public class ClassCalculator
+    {
+     public class Calculator
+        {        
+            
+            public double Square(double num) => num * num;
+
+            public double Add(double num1, double num2) => num1 + num2;
+            public double Multiply(double  num1, double num2) => num1 * num2;
+            public double Divide(double num1, double num2) => num2 / num1;
+            public double Subtract(double num1, double num2)
+            {
+                if (num1 > num2) 
+                {
+                    return num1 - num2;
+                }
+                return num2 - num1;
+            }
+
+        }
         }
     } 
 
